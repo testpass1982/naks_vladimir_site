d13d3ed9de549263d3f643936a91aed5356e9b55
Anatoliy Popov <popov.anatoly@gmail.com>
2019-05-13 16:22:25 +0300

    fix random documents to services page

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -3,6 +3,9 @@ import random
 
 def random_documents(request):
     all_documents = Document.objects.all()
-    all_document_pks = [doc.pk for doc in all_documents]
-    documents = [Document.objects.get(pk=random.choice(all_document_pks)) for i in range(0, 3)]
-    return {'random_documents': documents}
+    if len(all_documents) > 2:
+        all_document_pks = [doc.pk for doc in all_documents]
+        documents = [Document.objects.get(pk=random.choice(all_document_pks)) for i in range(0, 3)]
+        return {'random_documents': documents}
+    else:
+        return {'random_documents': ['Нет документов в базе данных']}
